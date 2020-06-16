6637f9ba69834291bd64636b7b28efbba83824da
Mirna Gama <mirnabeatrizgamav@gmail.com>
2020-06-16 15:48:22 -0300

    Elaboração de menu para administrar o sistema de notas

--- menu.py
+++ menu.py
@@ -1,8 +1,32 @@
 from sistema_notas import ScoreSystem
 
-s = System()
+s = ScoreSystem()
+value = 0
+print("== SISTEMA DE NOTAS ==")
 
-s.update("Maria", "10", "8", "7")
-s.read()
+while(value != "4"):
+    print("\n1) Inserir informações de aluno")
+    print("2) Calcular média de aluno")
+    print("3) Ler arquivo de notas")
+    print("4) Sair\n")
+    value = input()
 
-print(s.giveAverage("Maria"))
+    if (value == "1"):
+        name = input("Nome do aluno(a): ")
+        score1 = input("Nota da 1ª Prova: ")
+        score2 = input("Nota da 2ª Prova: ")
+        score3 = input("Nota da 3ª Prova: ")
+        s.update(name, score1, score2, score3)
+
+        print("\nArquivo atualizado com sucesso. Confira abaixo: ")
+        s.read()
+    elif (value == "2"):
+        student = input("Digite o nome do aluno: ")
+        print("Média: {0:.2f}\n".format(s.giveAverage(student)))
+    elif (value == "3"):
+        print("\nArquivo de Notas:")
+        s.read()
+    elif (value == "4"):
+        print("Saindo . . .")
+    else:
+        print("Digite uma opção válida")
